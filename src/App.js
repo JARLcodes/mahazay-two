d311f7f8ab2c7236f9faaf4015f3768198cba922
Janel Grant <janelgr5@gmail.com>
2018-05-03 18:19:46 -0400

    navbar's back baby

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
+import './App.css';
+import Navbar from './components/Navbar';
 import SingleEntry from './components/SingleEntry.jsx'
+
+const theme = createMuiTheme();
 
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <div className="col-xs-10">NAVBAR PLACEHOLDER</div>
-          <div className="col-xs-10">
-          <Switch>
-            <Route exact path="/" />
-            <Route exact path="/entries/:id" component={SingleEntry}/>
-          </Switch>
+        <MuiThemeProvider theme={theme}>
+          <div className="App">
+            <div className="col-xs-10">
+              <Navbar />
+            </div>
+            <div className="col-xs-10">
+            <Switch>
+              <Route exact path="/" />
+              <Route exact path="/entries/:id" component={SingleEntry}/>
+            </Switch>
+            </div>
           </div>
-        </div>
+        </MuiThemeProvider>
       </Router>
     );
   }
