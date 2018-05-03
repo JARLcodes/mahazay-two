eb0668aaf830df1863bb2855132e4de4b072defd
Abigail Demsas <ademsas1026@gmail.com>
2018-05-03 18:56:06 -0400

    styling toolbars render

--- src/components/SingleEntry.jsx
+++ src/components/SingleEntry.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { EditorState, RichUtils, convertFromRaw } from "draft-js";
+import { EditorState, RichUtils, convertFromRaw, convertToRaw } from "draft-js";
 import Editor, { composeDecorators } from 'draft-js-plugins-editor';
 import createImagePlugin from 'draft-js-image-plugin';
 import createFocusPlugin from 'draft-js-focus-plugin';
@@ -8,6 +8,7 @@ import Button from "material-ui/Button";
 
 const focusPlugin = createFocusPlugin();
 const blockDndPlugin = createBlockDndPlugin();
+
 
 const decorator = composeDecorators(
   focusPlugin.decorator,
@@ -26,15 +27,15 @@ const initialState = {
   "entityMap": {
       "0": {
           "type": "IMAGE",
-          "mutability": "IMMUTABLE",
+          "mutability": "MUTABLE",
           "data": {
-              "src": "/images/canada-landscape-small.jpg"
+              "src": "/images/cherry-blossom.jpg"
           }
       }
   },
   "blocks": [{
       "key": "9gm3s",
-      "text": "You can have images in your text field which are draggable. Hover over the image press down your mouse button and drag it to another position inside the editor.",
+      "text": "...start here",
       "type": "unstyled",
       "depth": 0,
       "inlineStyleRanges": [],
@@ -54,7 +55,7 @@ const initialState = {
       "data": {}
   }, {
       "key": "e23a8",
-      "text": "You can checkout the alignment tool plugin documentation to see how to build a compatible block plugin …",
+      "text": "",
       "type": "unstyled",
       "depth": 0,
       "inlineStyleRanges": [],
@@ -75,27 +76,66 @@ const styleMap = {
 };
 
 export default class SingleEntry extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createWithContent(convertFromRaw(initialState))};
-    this.onChange = (editorState) => this.setState({editorState});
+  state = {
+    editorState: EditorState.createWithContent(convertFromRaw(initialState)), 
+    alignment: 'left', 
+    showStyleToolbar: false, 
+    showAlignmentToolbar: false
+  }
+  
+  onChange = editorState => {
+    // to send data from entry to firebase WHILE USER IS UPDATING: use convertToRaw(editorState.getCurrentContent())
+    console.log(convertToRaw(editorState.getCurrentContent()))
+    this.setState({editorState})
   }
 
-  onStyleClick(style) {
+  toggleInlineStyle = style => () => 
     this.onChange(RichUtils.toggleInlineStyle(
       this.state.editorState,
       style
-    ));
+    ))
+
+  onBold = this.toggleInlineStyle('BOLD')
+  onItalic = this.toggleInlineStyle('ITALIC')
+  onUnderline = this.toggleInlineStyle('UNDERLINE')
+  onStrikethrough = this.toggleInlineStyle('STRIKETHROUGH')
+
+  onAlignmentChange(alignment){
+    this.setState({ alignment })
+  }
+
+  showStyleToolbar(){
+    this.setState({ showStyleToolbar: !this.state.showStyleToolbar })
+  }
+  showAlignmentToolbar(){
+    this.setState({ showAlignmentToolbar: !this.state.showAlignmentToolbar })
+  }
+
+  renderStyleToolbar() {
+    return <React.Fragment>
+        <Button onClick={this.onBold}>Bold</Button>
+        <Button onClick={this.onItalic}>Italic</Button>
+        <Button onClick={this.onUnderline}>Underline</Button>
+        <Button onClick={this.onStrikethrough}>Strikethrough</Button>
+    </React.Fragment>
+  }
+
+  renderAlignmentToolbar(){
+    return <React.Fragment>
+        <Button onClick={this.onAlignmentChange.bind(this, 'left')}>Left</Button>
+        <Button onClick={this.onAlignmentChange.bind(this, 'center')}>Center</Button>
+        <Button onClick={this.onAlignmentChange.bind(this, 'right')}>Right</Button>
+    </React.Fragment>
   }
 
   render() {
-    
+    const { alignment, showStyleToolbar, showAlignmentToolbar } = this.state;
     return (
       <div>
-        <Button onClick={this.onStyleClick.bind(this, 'BOLD')}>Bold</Button>
-        <Button onClick={this.onStyleClick.bind(this, 'ITALIC')}>Italic</Button>
-        <Button onClick={this.onStyleClick.bind(this, 'UNDERLINE')}>Underline</Button>
-        <Button onClick={this.onStyleClick.bind(this, 'STRIKETHROUGH')}>Strikethrough</Button>
+        <Button onClick={this.showStyleToolbar.bind(this)}><b>B</b><i>I</i><u>U</u></Button>
+        {showStyleToolbar && <div>{this.renderStyleToolbar()}</div>}
+        <Button onClick={this.showAlignmentToolbar.bind(this)}>Align</Button>
+        {showAlignmentToolbar && <div>{this.renderAlignmentToolbar()}</div>}
         <div>
           <Editor
             customStyleMap={styleMap}
@@ -103,6 +143,7 @@ export default class SingleEntry extends React.Component {
             onChange={this.onChange}
             placeholder="...start here"
             plugins={plugins}
+            textAlignment={'right'}
           />
         </div>
       </div>
