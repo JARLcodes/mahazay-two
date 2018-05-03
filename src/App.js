1089ac985c2929c741ec3f164b108771f317f717
Abigail Demsas <ademsas1026@gmail.com>
2018-05-03 17:14:11 -0400

    added routes (#41)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import SingleEntry from './components/SingleEntry.jsx'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="col-xs-10">NAVBAR PLACEHOLDER</div>
+          <div className="col-xs-10">
+          <Switch>
+            <Route exact path="/" />
+            <Route exact path="/entries/:id" component={SingleEntry}/>
+          </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
