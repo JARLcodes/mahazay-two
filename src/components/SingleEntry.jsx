4bba99133170941ab743db3e779971feb0fd3f27
Abigail Demsas <ademsas1026@gmail.com>
2018-05-03 17:13:10 -0400

    added routes

--- src/components/SingleEntry.jsx
+++ src/components/SingleEntry.jsx
@@ -1,6 +1,68 @@
 import React from "react";
-import {Editor, EditorState, RichUtils} from "draft-js";
-import Button from 'material-ui/Button';
+import { EditorState, RichUtils, convertFromRaw } from "draft-js";
+import Editor, { composeDecorators } from 'draft-js-plugins-editor';
+import createImagePlugin from 'draft-js-image-plugin';
+import createFocusPlugin from 'draft-js-focus-plugin';
+import createBlockDndPlugin from 'draft-js-drag-n-drop-plugin';
+import Button from "material-ui/Button";
+
+const focusPlugin = createFocusPlugin();
+const blockDndPlugin = createBlockDndPlugin();
+
+const decorator = composeDecorators(
+  focusPlugin.decorator,
+  blockDndPlugin.decorator
+);
+const imagePlugin = createImagePlugin({ decorator });
+
+const plugins = [
+  blockDndPlugin,
+  focusPlugin,
+  imagePlugin
+];
+
+/* eslint-disable */
+const initialState = {
+  "entityMap": {
+      "0": {
+          "type": "IMAGE",
+          "mutability": "IMMUTABLE",
+          "data": {
+              "src": "/images/canada-landscape-small.jpg"
+          }
+      }
+  },
+  "blocks": [{
+      "key": "9gm3s",
+      "text": "You can have images in your text field which are draggable. Hover over the image press down your mouse button and drag it to another position inside the editor.",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+  }, {
+      "key": "ov7r",
+      "text": " ",
+      "type": "atomic",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [{
+          "offset": 0,
+          "length": 1,
+          "key": 0
+      }],
+      "data": {}
+  }, {
+      "key": "e23a8",
+      "text": "You can checkout the alignment tool plugin documentation to see how to build a compatible block plugin …",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+  }]
+};
+/* eslint-enable */
 
 
 const styleMap = {
@@ -12,10 +74,10 @@ const styleMap = {
   }
 };
 
-export default class App extends React.Component {
+export default class SingleEntry extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {editorState: EditorState.createWithContent(convertFromRaw(initialState))};
     this.onChange = (editorState) => this.setState({editorState});
   }
 
@@ -40,6 +102,7 @@ export default class App extends React.Component {
             editorState={this.state.editorState}
             onChange={this.onChange}
             placeholder="...start here"
+            plugins={plugins}
           />
         </div>
       </div>
