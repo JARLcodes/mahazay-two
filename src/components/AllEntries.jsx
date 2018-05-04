15780810ea9733f252a3daf1abaa0ac4595b4157
Abigail Demsas <ademsas1026@gmail.com>
2018-05-04 14:27:42 -0400

    firebase set up

---
+++ src/components/AllEntries.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+// export const db = firebase.firestore(); defined in src/App.js
+import { db } from './../App';
+
+
+export default class AllEntries extends Component {
+  constructor(){
+    super();
+    this.state = {};
+  
+
+  }
+  componentDidMount(){
+   console.log('db.collection(entries)', db.collection('entries'))
+   //to add to database
+  //  db.collection('entries').add({
+  //    content: 'blahhhhhh', 
+  //    id: 'second entry'
+  //  })
+  //  .then(docRef => console.log('document written with id: ', docRef.id))
+  //  .catch(console.error)
+   //to get from database
+  //  db.collection('entries').get().then(querySnapshot => {
+  //    querySnapshot.forEach(doc => {
+  //      console.log(`${doc.id} => ${JSON.stringify(doc.data())}`)
+  //    })
+  //  })
+  }
+
+  
+  render() {
+    console.log('database', db._config.credentials.app.firebase_);
+    return (
+      <div>
+        <h1>here here hereeeee</h1>
+      </div>
+    )
+  }
+}
---
+++ src/utils/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfigs = {
+  apiKey: "AIzaSyCTEp2GTWkl3r3P3-gCuz45sN89ycD7Duo",
+  authDomain: "mahazay-c248c.firebaseapp.com",
+  databaseURL: "https://mahazay-c248c.firebaseio.com",
+  projectId: "mahazay-c248c",
+  storageBucket: "mahazay-c248c.appspot.com",
+  messagingSenderId: "255815640961"
+};
+
