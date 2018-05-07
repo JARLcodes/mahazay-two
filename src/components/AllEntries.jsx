f089774f86134ea6ed910c8c3235ae99c9776959
Abigail Demsas <ademsas1026@gmail.com>
2018-05-06 20:39:07 -0400

    can add entry from all entries page

--- src/components/AllEntries.jsx
+++ src/components/AllEntries.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import Button from "material-ui/Button";
 
-
 import { getRootRef } from '../utils/componentUtils';
-
 
 
 export default class AllEntries extends Component {
@@ -29,7 +27,7 @@ export default class AllEntries extends Component {
   addEntry(){
     console.log('assigning new entry to journal AND generating new entry id');
     const newEntryId = Number(this.state.allEntryIds[this.state.allEntryIds.length - 1]) + 1;
-    this.props.history.push(`/entries/${newEntryId}`)
+    this.props.history.push(`/entries/${newEntryId}`);
   }
 
   render() {
