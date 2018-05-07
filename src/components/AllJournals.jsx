f2950eae2f248632ebb9955ab26a35fb2a880394
Janel Grant <janelgr5@gmail.com>
2018-05-07 18:10:24 -0400

    all journals working

--- src/components/AllJournals.jsx
+++ src/components/AllJournals.jsx
@@ -2,9 +2,72 @@ import React, { Component } from 'react';
 import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
 import Subheader from 'material-ui/List/ListSubheader';
 import IconButton from 'material-ui/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
+import SubdirectoryArrowLeft from '@material-ui/icons/SubdirectoryArrowLeft';
+import { Link } from 'react-router-dom';
 
+import { getRootRef } from '../utils/componentUtils';
 import  NewJournalForm  from './NewJournalForm.jsx';
+
+
+export default class AllJournals extends Component {
+  constructor () {
+    super();
+    this.state = {
+      rootRef: getRootRef('journals'), 
+      journals: [], 
+      allJournalIds: []
+    }
+
+    // this.handleClick = this.handleClick.bind(this);
+  }
+  // handleClick (event) {
+  //   <Redirect to={`/journals/${event.target.value}`} />
+  // }
+
+  componentDidMount(){
+    const { rootRef } = this.state;
+    rootRef.get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(journal => {
+          this.setState({ journals: [...this.state.journals, journal.data()], allJournalIds: [...this.state.allJournalIds, journal.id] })
+        })
+      })
+  }
+
+  render() {
+    console.log(this.state.allJournalIds)
+    const journals = this.state.journals;
+    const journalIds = this.state.allJournalIds;
+    return (
+      <div style={styles.root}>
+        <GridList cellHeight={180} style={styles.gridList}>
+          <GridListTile key='Subheader' cols={2} style={{height: 'auto'}}>
+            <Subheader component="div">My Journals</Subheader>
+          </GridListTile>
+          {
+            journals.map((journal, ind) => (
+              <GridListTile key={journalIds[ind]} >
+                <Link to={`/journals/${journalIds[ind]}`}>
+                  <img src='https://cdn3.iconfinder.com/data/icons/design-flat-icons-vol-2/256/62-512.png' alt={journal.title} style={{height: 'auto'}}/>
+                  <GridListTileBar 
+                    title={journal.title}
+                    subtitle={<span>{journal.description}</span>}
+                    actionIcon={
+                      <IconButton style={styles.icon}>
+                        <SubdirectoryArrowLeft />
+                      </IconButton>
+                    }
+                  />
+                </Link>
+              </GridListTile>
+            ))
+          }
+        </GridList>
+        {/*<NewJournalForm />*/}
+      </div>
+    )
+  }
+}
 
 const styles = {
   root: {
@@ -22,18 +85,3 @@ const styles = {
     color: 'rgba(255, 255, 255, 0.54)',
   },
 };
-
-export default class AllJournals extends Component {
-  render() {
-    return (
-      <div style={styles.root}>
-        <GridList cellHeight={180} style={styles.gridList}>
-          <GridListTile key='Subheader' cols={2} style={{height: auto}}>
-            <Subheader component="div">My Journals</Subheader>
-          </GridListTile>
-        </GridList>
-        {/*<NewJournalForm />*/}
-      </div>
-    )
-  }
-}
