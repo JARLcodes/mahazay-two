0404369dbb0e59464ec290cd27339565e0143e25
Janel Grant <janelgr5@gmail.com>
2018-05-07 15:13:59 -0400

    all Journals started

--- src/components/AllJournals.jsx
+++ src/components/AllJournals.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
+import Subheader from 'material-ui/List/ListSubheader';
+import IconButton from 'material-ui/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+
 import  NewJournalForm  from './NewJournalForm.jsx';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    // backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+};
 
 export default class AllJournals extends Component {
   render() {
     return (
-      <div>
-        <NewJournalForm />
+      <div style={styles.root}>
+        <GridList cellHeight={180} style={styles.gridList}>
+          <GridListTile key='Subheader' cols={2} style={{height: auto}}>
+            <Subheader component="div">My Journals</Subheader>
+          </GridListTile>
+        </GridList>
+        {/*<NewJournalForm />*/}
       </div>
     )
   }
