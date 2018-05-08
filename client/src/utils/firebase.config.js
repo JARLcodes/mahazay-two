fe0a4d1f837376f3a5689a78234d3770a1c10dd8
Abigail Demsas <ademsas1026@gmail.com>
2018-05-08 10:54:56 -0400

    done

---
+++ client/src/utils/firebase.config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+export const firebaseConfigs = {
+  apiKey: "AIzaSyCTEp2GTWkl3r3P3-gCuz45sN89ycD7Duo",
+  authDomain: "mahazay-c248c.firebaseapp.com",
+  databaseURL: "https://mahazay-c248c.firebaseio.com",
+  projectId: "mahazay-c248c",
+  storageBucket: "mahazay-c248c.appspot.com",
+  messagingSenderId: "255815640961"
+};
+
+
+firebase.initializeApp(firebaseConfigs);
+const firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
+export const db = firestore;
+
