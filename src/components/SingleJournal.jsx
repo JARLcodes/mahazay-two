c6f48c771e5dc14462e39fc2a082fab2ef1773a4
Richi Thampan <richithampan@gmail.com>
2018-05-09 18:13:34 -0400

    single journal db pulls

--- src/components/SingleJournal.jsx
+++ src/components/SingleJournal.jsx
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {db} from '../utils/firebase.config'
+
+
+
+
 
 export default class SingleJournal extends Component {
   render() {
+    let entries = []
+    db.collection('entries').where("journalId", "==", this.props.match.params.id ).get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          entries.push(doc.data())
+        })
+      })
+    console.log("params",this.props.match.params.id ,"example:", entries)
     return (
       <div>
-        
+        hello there :)
       </div>
     )
   }
