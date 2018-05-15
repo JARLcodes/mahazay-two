e89a161035bea3e58c6d6c8387022d65a5bbe3f8
Lisa Shepard <lisashepard04@gmail.com>
2018-05-15 11:22:42 -0400

    work on styling

--- src/components/AllJournals.jsx
+++ src/components/AllJournals.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
+import Grid from 'material-ui/Grid';
 import Subheader from 'material-ui/List/ListSubheader';
 import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
-import SubdirectoryArrowLeft from '@material-ui/icons/SubdirectoryArrowLeft';
 import { Link } from 'react-router-dom';
 import { withAuth } from 'fireview';
 import Add from '@material-ui/icons/Add';
+import Card, {CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
 
 import { getRootRef } from '../utils/componentUtils';
 import  NewJournalForm  from './NewJournalForm.jsx';
@@ -19,10 +19,10 @@ const styles = {
     overflow: 'hidden',
   },
   gridList: {
-    borderStyle: 'inset',
-    borderWidth: 'thick',
-    borderColor: '#A1887F'
-  },
+    display: "flex",
+    flexWrap: "wrap",
+    overflowX: "auto",
+	},
   subheader: {
     fontSize: 40,
     fontVariant: 'small-caps',
@@ -46,62 +46,53 @@ export class AllJournals extends Component {
       rootRef: getRootRef('journals'),
       journals: [],
       allJournalIds: []
-    }
-
+    };
   }
 
-  componentWillReceiveProps(nextProps){
-    if(this.props._user !== nextProps._user){
+  componentWillReceiveProps(nextProps) {
+    if(this.props._user !== nextProps._user) {
       const { rootRef } = this.state;
       rootRef.where("userId", "==", nextProps._user.uid).get()
         .then(querySnapshot => {
           querySnapshot.forEach(journal => {
-            this.setState({ journals: [...this.state.journals, journal.data()], allJournalIds: [...this.state.allJournalIds, journal.id] })
-          })
-        })
+            this.setState({ journals: [...this.state.journals, journal.data()], allJournalIds: [...this.state.allJournalIds, journal.id] });
+          });
+        });
       }
   }
 
   newJournal(){
-    this.props.history.push(`${this.props._user.uid}/new-journal`)
+    this.props.history.push(`${this.props._user.uid}/new-journal`);
   }
 
   render() {
-  
     const journals = this.state.journals;
     const journalIds = this.state.allJournalIds;
+
     return (
-      <div style={styles.root}>
-        <GridList>
-          <GridListTile key='Subheader' cols={2} style={{height: 'auto'}}>
-            <Subheader style={styles.subheader}>My Journals</Subheader>
-            {this.props && this.props._user ? <Button  onClick={this.newJournal.bind(this)}><Add styles={styles.addJournal}/></Button> : null}
-          </GridListTile>
-         
+      <Grid>
+        <Grid container justify="center" spacing={24} style={styles.gridList}>
+          <Subheader style={styles.subheader}>My Journals</Subheader>
+            <Grid item xs={12}>{this.props && this.props._user ? <Button  onClick={this.newJournal.bind(this)}><Add styles={styles.addJournal}/></Button> : null}</Grid>
           {
             journals.map((journal, ind) => (
-              <GridListTile key={journalIds[ind]}>
+              <Grid key={journalIds[ind]}>
                 <Link to={`/journals/${journalIds[ind]}`}>
-                  <img src='https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/KB/HKB12/HKB12?wid=572&hei=572&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1480984401449'/>
-                  <GridListTileBar
-                    title={journal.title}
-                    subtitle={<span>{journal.description}</span>}
-                    actionIcon={
-                      <IconButton style={styles.icon}>
-                        <SubdirectoryArrowLeft />
-                      </IconButton>
-                    }
-                  />
+                <Card>
+                  <CardMedia 
+                    overlay={<CardTitle title={journal.title} subtitle={journal.description}/>}
+                  >
+                  <img src='https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/KB/HKB12/HKB12?wid=572&hei=572&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1480984401449' alt=''/>
+                  </CardMedia>
+                </Card>
                 </Link>
-              </GridListTile>
+              </Grid>
             ))
           }
-         
-        </GridList>
-      </div>
-    )
+         </Grid>
+        </Grid>
+    );
   }
 }
-
 
 export default withAuth(AllJournals)
