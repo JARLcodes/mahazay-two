49dcf993c144e23bec03056484a75c30362ab6ac
Lisa Shepard <lisashepard04@gmail.com>
2018-05-17 17:02:53 -0400

    whee

--- src/components/AllJournals.jsx
+++ src/components/AllJournals.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { getRootRef } from '../utils/componentUtils';
 import { withAuth } from 'fireview';
-import Grid from 'material-ui/Grid';
+import { Link } from 'react-router-dom';
+import { withTheme } from 'material-ui/styles';
 import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
 import Subheader from 'material-ui/List/ListSubheader';
 import Button from 'material-ui/Button';
-import { Link } from 'react-router-dom';
-import { withTheme } from 'material-ui/styles';
 import Add from '@material-ui/icons/Add';
-import image from '../images/whiteBook.jpg';
 import Typography from 'material-ui/Typography';
-import { NewJournalForm } from './index';
+import image from '../images/whiteBook.jpg';
 
-export class AllJournals extends Component {
+class AllJournals extends Component {
   constructor () {
     super();
     this.state = {
@@ -23,7 +21,7 @@ export class AllJournals extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props._user !== nextProps._user) {
+    if (this.props._user !== nextProps._user) {
       const { rootRef } = this.state;
       rootRef.where("userId", "==", nextProps._user.uid).get()
         .then(querySnapshot => {
@@ -44,17 +42,17 @@ export class AllJournals extends Component {
 
     return (
       <div style={{display: 'flex', flexWrap: 'nowrap', justifyContent: 'space-around', overflow: 'hidden'}}>
-        <GridList cellHeight="auto" style={{ spacing: "1em" }} cols={3}>
+        <GridList cellHeight="auto" style={{spacing: "1em"}} cols={3}>
         <GridListTile key="Subheader" cols={3}>
           
             {this.props && this.props._user && journals.length > 0
               ? <div>
-                  <Subheader component="div" variant="display1" style={{ fontSize: "2.5em", fontVariant: 'small-caps', color: 'grey' }}>Journals</Subheader> 
-                  <Button onClick={this.newJournal.bind(this)}><Add style={{ color: "grey", width: 30, height: "auto" }}/></Button>
+                  <Subheader component="div" variant="display1" style={{fontSize: "2.5em", fontVariant: 'small-caps', color: 'grey'}}>Journals</Subheader> 
+                  <Button onClick={this.newJournal.bind(this)}><Add style={{color: "grey", width: 30, height: "auto"}}/></Button>
                 </div> 
               : <div>
-                  <Subheader component="div" style={{ fontSize: "2.5em", fontVariant: 'small-caps', color: 'grey' }}>No Journals Made</Subheader>
-                  <Button onClick={this.newJournal.bind(this)}><Add style={{ color: "grey", width: 30, height: "auto" }}/></Button>
+                  <Subheader component="div" style={{fontSize: "2.5em", fontVariant: 'small-caps', color: 'grey'}}>No Journals Made</Subheader>
+                  <Button onClick={this.newJournal.bind(this)}><Add style={{color: "grey", width: 30, height: "auto"}}/></Button>
                 </div>
             }
           </GridListTile>
@@ -67,7 +65,7 @@ export class AllJournals extends Component {
                   subtitle={<Typography variant="subheading" style={{color: "#9E9E9E"}}>{journal.description}</Typography>}
                   titlePosition="top"
                 />
-                <img src={image} style={{ width: "60vh", height: "auto" }} alt=""/>
+                <img src={image} style={{width: "60vh", height: "auto"}} alt=""/>
                 </Link>
                 </GridListTile>
             ))
